ec1ef28c8630a0d6fb4f3803fbc8f096f844d8b2
unknown <cvlopes88@gmail.com>
2019-08-23 14:38:00 -0400

    someDone

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,71 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import { Link } from "react-router-dom";
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+ const CharacterList = props => {
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
+
+   const getCharacters = () => {
+    axios
+    .get(`https://rickandmortyapi.com/api/character/`)
+    .then(response => {
+      setCharacters(response.data.results);
+      console.log(response.data.results);
+  
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+  }
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    getCharacters();
   }, []);
 
   return (
     <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      
+      {characters.map(person => (
+        <CharacterData key={person.id} characters={person} />
+        
+      ))}
+
+    
+
     </section>
+    
   );
+  
+
+
+function CharacterData({characters}) {
+  const { name, gender, species, origin, status} = characters;
+  return (
+    <Link to={`/charater/${characters.id}`}>
+    <div className="cardList" >
+      <img src={characters.image}></img>
+      <div className="card">
+      Name:<h2>{characters.name}</h2>
+      </div>
+      <div className="card">
+      Gender:<h3>{characters.gender}</h3>
+      </div>
+      <div className="card">
+      species:<h3> {characters.species}</h3>
+      </div>
+      <div className="card">
+      Status:<h3>{characters.status}</h3>
+      </div>
+      
+
+      
+    </div>
+    </Link>
+  )
+  
 }
+ }
+export default CharacterList;
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
-
+import { Route } from 'react-router-dom';
+import CharacterList from './CharacterList';
+import WelcomePage from './WelcomePage';
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
-export default function TabNav() {
+//  TabNav 
 
-};
-
+const panes = [
+    { menuItem: 'Welcome', render: () => <WelcomePage>Tab 1 Content</WelcomePage> },
+    { menuItem: 'List of Characters', render: () => <CharacterList>Tab 2 Content</CharacterList> },
+    { menuItem: 'Location', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+  ]
+  
+  const TabNav = () => <Tab panes={panes} />
+  
+  export default TabNav;
